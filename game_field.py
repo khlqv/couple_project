a8e0483cd67db97802212ab508b90d3f8c53b5e8
yulibenyair <yulibenyair@gmail.com>
2026-09-08 13:06:27 +0300

    updated consts and game field. creates new field and fills with mines.

--- game_field.py
+++ game_field.py
@@ -1,4 +1,67 @@
 # game_field.py
 import consts
+import random
+
 flag_row = consts.BOARD_ROWS - consts.FLAG_ROWS
 flag_col = consts.BOARD_COLS - consts.FLAG_COLS
+
+field_grid = []
+
+
+# create matrix 20 on 50 and inserts free
+def create():
+    global field_grid
+    for row in range(consts.BOARD_ROWS):
+        field_grid.append([])
+        for col in range(consts.BOARD_COLS):
+            field_grid[row].append([])
+            field_grid[row][col] = consts.FREE
+
+
+def put_mine(row, col_start):
+    for col in range(col_start, col_start + 3):
+        field_grid[row][col] = consts.MINE
+
+
+def is_free_from_mine(row, col):
+    for i in range(3):
+        if field_grid[row][i + col] == consts.MINE:
+            return False
+    return True
+
+
+def fill_mines():
+    for i in range(consts.MINES_COUNT):
+        x_random = random.randint(consts.SOLDIER_COLS,
+                                  consts.BOARD_COLS - 3) - 1
+        y_random = random.randint(consts.SOLDIER_ROWS, consts.BOARD_ROWS) - 1
+        if field_grid[y_random][x_random] == consts.FREE and is_free_from_mine(y_random,x_random):
+            put_mine(y_random, x_random)
+        else:
+            while field_grid[y_random][x_random] != consts.FREE or not is_free_from_mine(y_random,x_random):
+                x_random = random.randint(consts.SOLDIER_COLS,
+                                          consts.BOARD_COLS)
+                y_random = random.randint(consts.SOLDIER_ROWS,
+                                          consts.BOARD_ROWS)
+            put_mine(y_random, x_random)
+
+
+def put_flag():
+    for row in range(consts.BOARD_COLS - consts.FLAG_COLS,
+                     consts.BOARD_COLS):
+        for col in range(consts.BOARD_ROWS - consts.FLAG_ROWS,
+                         consts.BOARD_ROWS):
+            field_grid[col][row] = consts.FLAG
+
+
+def print_matrix(matrix):
+    for row in matrix:
+        for col in row:
+            print(col, end=" ")
+        print()
+
+
+create()
+fill_mines()
+put_flag()
+print_matrix(field_grid)
